a9c1a04906894d0464f3abeb7ab92f0aaf713b46
linwexEU <130746152+linwexEU@users.noreply.github.com>
2023-12-26 10:31:39 +0200

    Update fastfunc.py

--- app/function/fastfunc.py
+++ app/function/fastfunc.py
@@ -7,7 +7,7 @@ async def get_id_test(user_id: int):
 
 
 async def get_text_from_file(test_id: int) -> list:  
-    with open(f"all_tests/vseobot-question{test_id}.txt", encoding="utf-8") as file:
+    with open(fr"all_tests\vseobot-question{test_id}.txt", encoding="utf-8") as file:
         file_content = [line.split("\n") for line in file.read().split("-" * 80)]
          
     return [[line for line in item if line != ""] for item in file_content if item != []][:-1]
